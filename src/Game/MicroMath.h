bdd9d1f927f0839bce2f79a2ab1552605fdbb20f
warzes <warzes@mail.ru>
2023-02-01 01:04:06 +1000

    - Vector2 - Vector3

--- src/Game/MicroMath.h
+++ src/Game/MicroMath.h
@@ -33,6 +33,15 @@ inline bool Ñompare(float f1, float f2) { return fabsf(f1 - f2) < EPSILON; }
 class Point2
 {
 public:
+	constexpr Point2() = default;
+	constexpr Point2(Point2&&) = default;
+	constexpr Point2(const Point2&) = default;
+	constexpr Point2(int n) : x(n), y(n) {}
+	constexpr Point2(int nx, int ny) : x(nx), y(ny) {}
+
+	constexpr Point2& operator=(Point2&&) = default;
+	constexpr Point2& operator=(const Point2&) = default;
+
 	int x = 0;
 	int y = 0;
 };
@@ -43,6 +52,14 @@ public:
 class Color
 {
 public:
+	constexpr Color() = default;
+	constexpr Color(Color&&) = default;
+	constexpr Color(const Color&) = default;
+	constexpr Color(unsigned char nr, unsigned char ng, unsigned char nb, unsigned char na = 255) : r(nr), g(ng), b(nb), a(na) {}
+
+	constexpr Color& operator=(Color&&) = default;
+	constexpr Color& operator=(const Color&) = default;
+
 	unsigned char r = 0;
 	unsigned char g = 0;
 	unsigned char b = 0;
@@ -55,9 +72,82 @@ public:
 class Vector2
 {
 public:
+	constexpr Vector2() = default;
+	constexpr Vector2(Vector2&&) = default;
+	constexpr Vector2(const Vector2&) = default;
+	constexpr Vector2(float num) : x(num), y(num) {}
+	constexpr Vector2(float nx, float ny) : x(nx), y(ny) {}
+
+	constexpr Vector2& operator=(Vector2&&) = default;
+	constexpr Vector2& operator=(const Vector2&) = default;
+
+	friend Vector2 operator+(const Vector2& u, float num) { return { u.x + num, u.y + num }; }
+	friend Vector2 operator+(float num, const Vector2& u) { return { num + u.x, num + u.y }; }
+	friend Vector2 operator+(const Vector2& u, const Vector2& v) { return { u.x + v.x, u.y + v.y }; }
+
+	Vector2 operator-() const { return { -x, -y }; }
+	friend Vector2 operator-(const Vector2& u, float num) { return { u.x - num, u.y - num }; }
+	friend Vector2 operator-(float num, const Vector2& u) { return { num - u.x, num - u.y }; }
+	friend Vector2 operator-(const Vector2& u, const Vector2& v) { return { u.x - v.x, u.y - v.y }; }
+
+	friend Vector2 operator*(const Vector2& u, float num) { return { u.x * num, u.y * num }; }
+	friend Vector2 operator*(float num, const Vector2& u) { return { num * u.x, num * u.y }; }
+	friend Vector2 operator*(const Vector2& u, const Vector2& v) { return { u.x * v.x, u.y * v.y }; }
+
+	friend Vector2 operator/(const Vector2& u, float num) { return { u.x / num, u.y / num }; }
+	friend Vector2 operator/(float num, const Vector2& u) { return { num / u.x, num / u.y }; }
+	friend Vector2 operator/(const Vector2& u, const Vector2& v) { return { u.x / v.x, u.y / v.y }; }
+
+	Vector2& operator+=(float num) { x += num; y += num; return *this; }
+	Vector2& operator+=(const Vector2& u) { x += u.x; y += u.y; return *this; }
+	Vector2& operator-=(float num) { x -= num; y -= num; return *this; }
+	Vector2& operator-=(const Vector2& u) { x -= u.x; y -= u.y; return *this; }
+	Vector2& operator*=(float num) { x *= num; y *= num; return *this; }
+	Vector2& operator*=(const Vector2& u) { x *= u.x; y *= u.y; return *this; }
+	Vector2& operator/=(float num) { x /= num; y /= num; return *this; }
+	Vector2& operator/=(const Vector2& u) { x /= u.x; y /= u.y; return *this; }
+
+	constexpr float& operator[](size_t i) { return (&x)[i]; }
+	constexpr const float operator[](size_t i) const { return (&x)[i]; }
+
+	[[nodiscard]] float GetLength() const;
+	[[nodiscard]] float GetLengthSquared() const;
+	[[nodiscard]] Vector2 Normalize() const;
+
 	float x = 0.0f;
 	float y = 0.0f;
 };
+
+inline float Vector2::GetLength() const
+{
+	return sqrtf(x * x + y * y);
+}
+
+inline float Vector2::GetLengthSquared() const
+{
+	return x * x + y * y;
+}
+
+inline Vector2 Vector2::Normalize() const
+{
+	const float invLen = 1.0f / sqrtf(x * x + y * y);
+	return { x * invLen, y * invLen };
+}
+
+inline float Distance(const Vector2& v1, const Vector2& v2)
+{
+	return (v1 - v2).GetLength();
+}
+
+inline float DotProduct(const Vector2& v1, const Vector2& v2)
+{
+	return v1.x * v2.x + v1.y * v2.y;
+}
+
+inline Vector2 Mix(const Vector2& u, const Vector2& v, float a)
+{
+	return u * (1.0f - a) + v * a;
+}
 
 //=============================================================================
 // Vector3
@@ -65,10 +155,118 @@ public:
 class Vector3
 {
 public:
+	static const Vector3 Zero;
+	static const Vector3 One;
+	static const Vector3 Right;
+	static const Vector3 Up;
+	static const Vector3 Forward;
+
+	constexpr Vector3() = default;
+	constexpr Vector3(Vector3&&) = default;
+	constexpr Vector3(const Vector3&) = default;
+	constexpr Vector3(float n) : x(n), y(n), z(n) {}
+	constexpr Vector3(float nx, float ny, float nz) : x(nx), y(ny), z(nz) {}
+
+	constexpr Vector3& operator=(Vector3&&) = default;
+	constexpr Vector3& operator=(const Vector3&) = default;
+
+	friend Vector3 operator+(const Vector3& u, float num) { return { u.x + num, u.y + num, u.z + num }; }
+	friend Vector3 operator+(float num, const Vector3& u) { return { num + u.x, num + u.y, num + u.z }; }
+	friend Vector3 operator+(const Vector3& u, const Vector3& v) { return { u.x + v.x, u.y + v.y, u.z + v.z }; }
+	constexpr Vector3 operator-() const { return { -x, -y, -z }; }
+	friend Vector3 operator-(const Vector3& u, float num) { return { u.x - num, u.y - num, u.z - num }; }
+	friend Vector3 operator-(float num, const Vector3& u) { return { num - u.x, num - u.y, num - u.z }; }
+	friend Vector3 operator-(const Vector3& u, const Vector3& v) { return { u.x - v.x, u.y - v.y, u.z - v.z }; }
+	friend Vector3 operator*(const Vector3& u, float num) { return { u.x * num, u.y * num, u.z * num }; }
+	friend Vector3 operator*(float num, const Vector3& u) { return { num * u.x, num * u.y, num * u.z }; }
+	friend Vector3 operator*(const Vector3& u, const Vector3& v) { return { u.x * v.x, u.y * v.y, u.z * v.z }; }
+	friend Vector3 operator/(const Vector3& u, float num) { return { u.x / num, u.y / num, u.z / num }; }
+	friend Vector3 operator/(float num, const Vector3& u) { return { num / u.x, num / u.y, num / u.z }; }
+	friend Vector3 operator/(const Vector3& u, const Vector3& v) { return { u.x / v.x, u.y / v.y, u.z / v.z }; }
+
+	Vector3& operator+=(float num) { x += num; y += num; z += num; return *this; }
+	Vector3& operator+=(const Vector3& u) { x += u.x; y += u.y; z += u.z; return *this; }
+	Vector3& operator-=(float num) { x -= num; y -= num; z -= num; return *this; }
+	Vector3& operator-=(const Vector3& u) { x -= u.x; y -= u.y; z -= u.z; return *this; }
+	Vector3& operator*=(float num) { x *= num; y *= num; z *= num; return *this; }
+	Vector3& operator*=(const Vector3& u) { x *= u.x; y *= u.y; z *= u.z; return *this; }
+	Vector3& operator/=(float num) { x /= num; y /= num; z /= num; return *this; }
+	Vector3& operator/=(const Vector3& u) { x /= u.x; y /= u.y; z /= u.z; return *this; }
+
+	constexpr float& operator[](size_t i) { return (&x)[i]; }
+	constexpr const float operator[](size_t i) const { return (&x)[i]; }
+
+	[[nodiscard]] float GetLength() const;
+	[[nodiscard]] float GetLengthSquared() const;
+	[[nodiscard]] Vector3 Normalize() const;
+
 	float x = 0.0f;
 	float y = 0.0f;
 	float z = 0.0f;
 };
+
+inline const Vector3 Vector3::Zero = { 0.0f };
+inline const Vector3 Vector3::One = { 1.0f };
+inline const Vector3 Vector3::Right = { 1.0f, 0.0f, 0.0f };
+inline const Vector3 Vector3::Up = { 0.0f, 1.0f, 0.0f };
+inline const Vector3 Vector3::Forward = { 0.0f, 0.0f, 1.0f };
+
+inline float Vector3::GetLength() const
+{
+	return sqrtf(x * x + y * y + z * z);
+}
+
+inline float Vector3::GetLengthSquared() const
+{
+	return x * x + y * y + z * z;
+}
+
+inline Vector3 Vector3::Normalize() const
+{
+	const float invLen = 1.0f / sqrtf(x * x + y * y + z * z);
+	return { x * invLen, y * invLen, z * invLen };
+}
+
+inline float Distance(const Vector3& v1, const Vector3& v2)
+{
+	return (v1 - v2).GetLength();
+}
+
+inline float DotProduct(const Vector3& v1, const Vector3& v2)
+{
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+inline Vector3 CrossProduct(const Vector3& v1, const Vector3& v2)
+{
+	return {
+		v1.y * v2.z - v1.z * v2.y,
+		v1.z * v2.x - v1.x * v2.z,
+		v1.x * v2.y - v1.y * v2.x
+	};
+}
+
+inline Vector3 Min(const Vector3& v1, const Vector3& v2)
+{
+	return {
+		Min(v1.x, v2.x),
+		Min(v1.y, v2.y),
+		Min(v1.z, v2.z),
+	};
+}
+inline Vector3 Max(const Vector3& v1, const Vector3& v2)
+{
+	return {
+		Max(v1.x, v2.x),
+		Max(v1.y, v2.y),
+		Max(v1.z, v2.z),
+	};
+}
+
+inline Vector3 Lerp(const Vector3& a, const Vector3& b, float x)
+{
+	return a + (b - a) * x;
+}
 
 //=============================================================================
 // Matrix4
